8669fd8559dd29c5f43f693ffef935d84f9ef054
dannistoneca <dannistoneca@gmail.com>
2025-06-23 06:09:15 -0300

    Add test cases for admin banner variations

--- tests/AdminBannerTest.php
+++ tests/AdminBannerTest.php
@@ -20,6 +20,20 @@ class AdminBannerTest extends TestCase {
 	}
 
 	/**
+	 * Banner should not appear when WordPress runs in a subdirectory but home page is root.
+	 */
+	public function test_banner_not_needed_when_home_is_root() {
+		$this->assertFalse( should_show_admin_banner( 'https://example.com/wp/', 'https://example.com/' ) );
+	}
+
+	/**
+	 * Banner should not appear when WordPress is root but home page is in a subdirectory.
+	 */
+	public function test_banner_not_needed_when_site_root_and_home_subdirectory() {
+		$this->assertFalse( should_show_admin_banner( 'https://example.com/', 'https://example.com/blog/' ) );
+	}
+
+	/**
 	 * Banner should not appear for root installs.
 	 */
 	public function test_banner_not_needed_when_site_root() {
